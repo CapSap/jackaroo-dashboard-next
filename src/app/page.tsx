3e1824d76157b4bc80fd2f367e553dc52e37dee1
CapSap <cshotam@gmail.com>
2024-10-04 00:06:06 +1000

    remove log

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,6 @@ export default async function Home() {
   const supabase = createClient();
   const { data: notes } = await supabase.from("notes").select();
 
-  console.log("LOG", JSON.stringify(notes));
-
   return (
     <div>
       <Upload />
