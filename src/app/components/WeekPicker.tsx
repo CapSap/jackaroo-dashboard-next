29b306dbeea5d53ccf563780f650a0666a096ca6
CapSap <cshotam@gmail.com>
2024-10-05 12:12:17 +1000

    rename dashboard to weekpicker

--- src/app/components/WeekPicker.tsx
+++ src/app/components/WeekPicker.tsx
@@ -3,7 +3,7 @@
 import { useEffect, useState } from "react";
 import { format, startOfWeek, endOfWeek, isSameWeek, subWeeks } from "date-fns";
 
-export default function Dashboard() {
+export default function WeekPicker() {
   const [selectedDate, setSelectedDate] = useState<{
     label: string;
     start: Date;
