9186fbcd346a798967aa7bc63e308b218e2ac459
CapSap <cshotam@gmail.com>
2024-10-05 23:36:52 +1000

    delete supabase test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,6 @@ import Upload from "./components/Upload";
 import WeekPicker from "./components/WeekPicker";
 
 export default async function Home() {
-  const supabase = createSupabaseClient();
-  const { data: notes } = await supabase.from("viare_shipped_orders").select();
-
-  console.log(notes);
-
   return (
     <div>
       home page
