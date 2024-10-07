dc0b00841bfd1217baa96278f147a99ddc65be99
CapSap <cshotam@gmail.com>
2024-10-07 12:34:40 +1100

    display order totals

--- src/app/components/Card.tsx
+++ src/app/components/Card.tsx
@@ -1,3 +1,36 @@
-export default function Card() {
-  return <div>vard</div>;
+export default function Card({ orders }: { orders: Order[] }) {
+  console.log(orders);
+  const title = orders[0].dispatch_point;
+
+  const expressOrders = orders.filter((order) => order.freight === 15);
+
+  const cncOrders = orders.filter(
+    (order) => order.shipped_to === "Paddy Pallin Sydney CBD"
+  );
+
+  const cncOrdersPostedOut = orders.filter(
+    (order) =>
+      order.shipped_to &&
+      new RegExp(`Paddy Pallin(?!.*${title})`).test(order.shipped_to)
+  );
+
+  const regOrderCount =
+    orders.length - (expressOrders.length + cncOrders.length);
+
+  console.log(cncOrders);
+
+  return (
+    <div>
+      <h3>{title} has achieved the following</h3>
+      <ul>
+        <li>Total viare orders processed: {orders.length}</li>
+        <li>Express shipments: {expressOrders.length}</li>
+        <li>Regular shipments: {regOrderCount}</li>
+        <li>CNCs posted out: {cncOrdersPostedOut.length}</li>
+        <li>
+          CNCs to be collected from {title}: {cncOrders.length}
+        </li>
+      </ul>
+    </div>
+  );
 }
